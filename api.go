d71a9b76ec66064b9faf249b61f14a96c4bdb497
alahirajeffrey <alahirajeffrey@gmail.com>
2024-06-11 18:40:58 +0100

    completed funcs to write response to json, conver handlers func to http handlers

--- api.go
+++ api.go
@@ -1,7 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func WriteJson (w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func (http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandlerFunc (f apiFunc) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request)  {
+		if err := f(w, r); err != nil{
+			// handle error
+			WriteJson(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
 type APIServer struct {
 	listenAddr string
 }
@@ -10,6 +35,12 @@ func NewAPIServer(listenAddr string) *APIServer {
 	return &APIServer{
 		listenAddr: listenAddr,
 	}
+}
+
+func (s *APIServer) Run () {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandlerFunc(s.handleAccount))
 }
 
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
